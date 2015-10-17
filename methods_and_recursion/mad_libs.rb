e9905a8a0b9ce08781ab40b01191c93f0f684140
Ryan <ryantnance@mac.com>
2015-10-17 14:33:57 -0600

    Added "insert_noun" (mad_libs.rb
    [#105166388]

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+def ask_question(question)
+  print "#{question}"
+  gets.chomp
+end
+
+adjective_1 = ask_question('Give me an adjective: ')
+adjective_2 = ask_question('Give me a another adjective: ')
+noun_1 = ask_question('Give me a noun: ')
+noun_2 = ask_question('Give me a another noun: ')
+plural_noun_1 = ask_question('Give me a plural noun: ')
+game = ask_question('Give me a game: ')
+plural_noun_2 = ask_question('Give me a another plural noun: ')
+verb_ending_in_ing_1 = ask_question('Give me a verb ending in "ing": ')
+verb_ending_in_ing_2 = ask_question('Give me another verb ending in "ing": ')
+plural_noun_3 = ask_question('Give me a plural noun: ')
+verb_ending_in_ing_3 = ask_question('Give me another verb ending in "ing": ')
+noun_3 = ask_question('Give me another noun: ')
+plant = ask_question('Give me a plant: ')
+part_of_the_body = ask_question('Give me a part of the body: ')
+place = ask_question('Give me a place: ')
+verb_ending_in_ing_4 = ask_question('Give me another verb ending in "ing":')
+adjective_3 = ask_question('Give me another adjective: ')
+number = ask_question('Give me a number: ')
+plural_noun_4 = ask_question('Give me a plural noun: ')
+
+puts "A vacation is when you take a trip to some #{adjective_1} place
+with your #{adjective_2} family. Usually you go to some place
+that is near a/an #{noun_1} or up on a/an #{noun_2}.
+A good vacation place is one where you can ride #{plural_noun_1}
+or play #{game} or go hunting for #{plural_noun_2}. I like
+to spend my time #{verb_ending_in_ing_1} or #{verb_ending_in_ing_2}.
+When parents go on a vacation, they spend their time
+eating three #{plural_noun_3} a day, and fathers play golf, and mothers
+sit around #{verb_ending_in_ing_3}. Last summer, my little brother
+fell in a/an #{noun_3} and got poison #{plant} all
+over his #{part_of_the_body}. My family is going to (the)
+#{place}, and I will practice #{verb_ending_in_ing_4}. Parents
+need vacations more than kids because parents are always very
+#{adjective_3} and because they have to work #{number}
+hours every day all year making enough #{plural_noun_4} to pay
+for the vacation."
